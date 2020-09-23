be8a71339e0feca0a30564a7d9feb524e6a3a7a5
uKiJo <nes-you-ukijo@hotmail.fr>
2020-09-23 13:13:15 +0100

    Updates

---
+++ precache-manifest.0b7e4e8fd6d97b92416fa4fdd1b89bd6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5aa68d1a4926b0e2c773120bc9610da",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "d40259c6ee43962d7559",
+    "url": "/my-app/static/css/main.d0b594c8.chunk.css"
+  },
+  {
+    "revision": "8961004a6b589eb17a4c",
+    "url": "/my-app/static/js/2.e1a0f1bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.e1a0f1bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d40259c6ee43962d7559",
+    "url": "/my-app/static/js/main.13100e6d.chunk.js"
+  },
+  {
+    "revision": "833d64ac4bae055be974",
+    "url": "/my-app/static/js/runtime-main.58a6d01e.js"
+  }
+]);
